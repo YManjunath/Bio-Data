7954f128b492ef64e2d1f612024998e114984f61
YManjunath <manjuyatnatti@gmail.com>
2025-03-19 18:11:48 +0530

    new updates

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const imagePath = [
+    'assets/IMG_9.JPG',
+    'assets/IMG_11.JPG',
+    'assets/IMG_12.JPG',
+    'assets/IMG_14.JPG',
+    'assets/IMG_13.JPG',
     'assets/IMG_4.JPG',
     'assets/IMG_6.JPG',
     'assets/IMG_5.JPG',
